0beb547850d83d382b858c2617e6473aeae4a0e6
Buppi Khan <buppikhan@Buppis-MacBook-Air.local>
2022-09-18 12:28:30 -0400

    fixed homepage image issue

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -13,13 +13,15 @@ const Header = () => {
           <input type="email" placeholder="Your Email Address"/>
           <button type="button">Get Started</button>
         </div>
+        
         <div className="gpt3__header-content__people">
           <img src={people} alt="people "/>
           <p>1,600 people requested a visit in last 24 hours</p>
         </div>
-        <div className="gpt3__header-image">
-          <img src={ai} alt="ai"/>
-        </div>
+        
+      </div>
+      <div className="gpt3__header-image">
+        <img src={ai} alt="ai"/>
       </div>
     </div>
   )
