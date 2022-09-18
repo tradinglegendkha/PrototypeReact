2639df7c12751b7cc7aab82fa43a75cec14ca0b4
Buppi Khan <buppikhan@Buppis-MacBook-Air.local>
2022-09-18 11:59:00 -0400

    touched header and edited home page

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
 import './header.css';
+import people from '../../assets/people.png';
+import ai from '../../assets/ai.png';
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className="gpt3__header section__padding" id="home">
+      <div className="gpt3__header-content">
+        <h1 className="gradient__text">Let's Build Something amazing with OpenAI</h1>
+        <p>The Future of AI is near and ready to rebel. With fast and efficient machine learning algorithm that beats out anything on this planet</p>
+        <div className="gpt3__header-content__input">
+          <input type="email" placeholder="Your Email Address"/>
+          <button type="button">Get Started</button>
+        </div>
+        <div className="gpt3__header-content__people">
+          <img src={people} alt="people "/>
+          <p>1,600 people requested a visit in last 24 hours</p>
+        </div>
+        <div className="gpt3__header-image">
+          <img src={ai} alt="ai"/>
+        </div>
+      </div>
+    </div>
   )
 }
 
