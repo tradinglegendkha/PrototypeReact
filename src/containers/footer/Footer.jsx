01a0a57f5a4ff7be488d744788088caa1f930db0
Buppi Khan <buppikhan@Buppis-MacBook-Air.local>
2022-09-21 17:38:29 -0400

    done with footer

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -1,9 +1,39 @@
 import React from 'react'
 import './footer.css';
+import gpt3Logo from '../../assets/logo.svg'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className="gpt3__footer section__padding">
+      <div className="gpt3__footer-heading">
+        <h1 className="gradient__text">Do you want to step into the future before others?</h1>
+      </div>
+      <div className="gpt3__footer-btn">
+        <p>Request Early Access</p>
+      </div>
+      <div className="gpt3__footer-links">
+        <div className="gpt3__footer-links_logo">
+          <img src={gpt3Logo} alt="logo"/>
+          <p>Copyright reserved</p>
+        </div>
+        <div className="gpt3__footer-links_div">
+          <h4>Links</h4>
+          <p>Overons</p>
+          <p>Social Media</p>
+          <p>Counters</p>
+          <p>Contact</p>
+        </div>
+        <div className="gpt3__footer-links_div">
+          <h4>Cimpany</h4>
+          <p>Terms and Conditions</p>
+          <p>Privacy Policy</p>
+          <p>Contact</p>
+        </div>
+      </div>
+      <div className="gpt3__footer-copyright">
+        <p>2021 GPT-3. All rights reserved.</p>
+      </div>
+    </div>
   )
 }
 
